b47f0c4d5ccb09db16bb3606bb66bc0b2f74e2c3
Felix Li <lbc1125wj@gmail.com>
2021-04-14 07:29:02 -0700

    new menu UI design

--- src/scene/Menu.js
+++ src/scene/Menu.js
@@ -7,34 +7,51 @@ class Menu extends Phaser.Scene
     }
     preload()
     {
+        this.load.image("title", "assets/title.jpg");
+        this.load.image("gamemode", "assets/gamemode.jpg");
         this.load.audio("sfx_select", "assets/blip_select12.wav");
         this.load.audio("sfx_explode", "assets/explosion38.wav");
         this.load.audio("sfx_shoot", "assets/rocket_shot.wav");
         this.load.audio("background_music", "assets/background.wav");
     }
-    BeginPlay()
+    OnePlayer()
     {
         this.scene.start("playScene");
     }
 
     create()
     {
-        let menuConfig = {
+        // let menuConfig = {
+        //     fontFamily: 'Courier',
+        //     fontSize: '42px',
+        //     backgroundColor: '#DEFACE',
+        //     color: '#843605',
+        //     align: 'center',
+        //     padding: {
+        //         top: 5,
+        //         bottom: 5,
+        //     },
+        //     fixedWidth: 600,
+        //     fixedHeight: 75
+        // }
+
+
+        let infoConfig = {
             fontFamily: 'Courier',
-            fontSize: '42px',
+            fontSize: '20px',
             backgroundColor: '#DEFACE',
             color: '#843605',
-            align: 'center',
+            align: 'left',
             padding: {
                 top: 5,
                 bottom: 5,
             },
-            fixedWidth: 500
+            fixedWidth: 300
         }
 
-        let infoConfig = {
+        let opConfig = {
             fontFamily: 'Courier',
-            fontSize: '23px',
+            fontSize: '16px',
             backgroundColor: '#F3B141',
             color: '#843605',
             align: 'center',
@@ -42,17 +59,50 @@ class Menu extends Phaser.Scene
                 top: 5,
                 bottom: 5,
             },
-            fixedWidth: 300
+            fixedWidth: 175
         }
-        //Set Menu info text
-        this.add.text(game.config.width/2, 100, "Rocket Patrol Menu", menuConfig).setOrigin(0.5);
-        this.add.text(20, 20, "'A' - Left Move    'D' - Right Move").setOrigin(-0.4, -15);
-        this.add.text(20, 20, "'K' - Shoot").setOrigin(-2.3, -18);
-        this.add.text(game.config.width/2, 400, "Click 'Enter' to play!", infoConfig).setOrigin(0.5);
-        
 
+        let opConfig2 = {
+            fontFamily: 'Courier',
+            fontSize: '16px',
+            backgroundColor: '#DEFACE',
+            color: '#843605',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 175
+        }
+        //add title
+        this.add.tileSprite(
+            20, 20, 600, 75, "title"
+        ).setOrigin(0);
+        //Set Menu info text
+        //Keyboard mode info
+        this.add.text(game.config.width/5, 150, "Keyboard Mode", opConfig2).setOrigin(0.5);
+        this.add.text(game.config.width/5, 175, "'A' - Move Left", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5, 200, "'D' - Move Right", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5, 225, "'K' - Shoot", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5, 250, "'N' - Switch Mode", opConfig).setOrigin(0.5);
+        //Mouse mode info
+        this.add.text(game.config.width/5 + 200, 150, "Mouse Mode", opConfig2).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 200, 175, "Move your mouse", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 200, 200, "to control the", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 200, 225, "rocket, click left", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 200, 250, "button to shoot!", opConfig).setOrigin(0.5);
+        //Co-op 2P info
+        this.add.text(game.config.width/5 + 400, 150, "Co-op 2P Control", opConfig2).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 400, 175, "'<-' - Move Left", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 400, 200, "'->' - Move Right", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 400, 225, "'Enter' - Shoot", opConfig).setOrigin(0.5);
+        this.add.text(game.config.width/5 + 400, 250, "Can't switch mode!", opConfig).setOrigin(0.5);
+        
+        this.add.tileSprite(
+            game.config.width/2, 375, 300, 150, "gamemode"
+        ).setOrigin(0.5);
         //Handle Scene Transition
-        this.input.keyboard.on('keydown-ENTER', this.BeginPlay, this);
+        this.input.keyboard.on('keydown-ONE', this.OnePlayer, this);
     }
 
 }
